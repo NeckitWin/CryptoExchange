302a0b12b39106c2635e521ce1ffbcb301a996b0
NeckitWin <neckit.v@gmail.com>
2024-03-04 02:02:00 +0100

    push all

---
+++ src/components/Input.vue
@@ -0,0 +1,57 @@
+<script>
+  export default {
+    props: {
+      changeAmount: {
+        type: Function,
+        required: true
+      },
+      convert: {
+        type: Function,
+        required: true
+      },
+      error: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<template>
+  <input min="0" type="number" @input="changeAmount($event.target.value)" placeholder="Enter a number" ><br>
+  <button @click="convert()">Convert</button>
+  <p v-if="error != ''">{{ error }}</p>
+</template>
+
+<style scoped>
+p{
+  color: #FF0000;
+}
+input {
+  margin-top: 10px;
+  width: 500px;
+  border-radius: 10px;
+  border: 0;
+  padding: 10px 15px;
+  color: #5d5d5d;
+  outline: none;
+  font-size: 1.2em;
+}
+
+button {
+  margin-top: 30px;
+  padding: 15px 20px;
+  color: #FFFFFF;
+  text-shadow: 0 0 5px #FFFFFF;
+  background: #1A032D;
+  border: 0;
+  text-transform: uppercase;
+  cursor: pointer;
+  border-radius: 3px;
+  transition: 0.5s ease;
+}
+button:hover {
+  background: #24043e;
+  transform: scale(1.05);
+}
+</style>
---
+++ src/components/Selector.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  props: {
+    setCrypto: {
+      type: Function,
+      required: true
+    }
+  },
+  data() {
+    return {
+      current: ''
+    }
+  },
+  methods: {
+    selectItem(val) {
+      this.setCrypto(val)
+      this.current = val
+    }
+  }
+
+}
+</script>
+
+<template>
+  <ul>
+    <li @click="selectItem('BTC')" :className="current == 'BTC' ? 'active' : ''">Bitcoin</li>
+    <li @click="selectItem('ETH')" :class="current == 'ETH' ? 'active' : ''">ETH</li>
+    <li @click="selectItem('USDT')" :class="current == 'USDT' ? 'active' : ''">USDT</li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  width: 200px;
+  padding: 0;
+  border-radius: 3px;
+  border: 3px solid #000;
+}
+
+li{
+  display: block;
+  background: #1A032D;
+  color: #fff;
+  padding: 20px 0;
+  transition: 0.5s ease;
+}
+
+li:hover, li.active {
+  background: #24043e;
+  cursor: pointer;
+  transform: scale(1.05);
+}
+</style>
